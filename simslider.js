629347ea96127372b66a0d01d7033a38639723dd
willi-dev <willi.ilmukomputer@gmail.com>
2015-06-29 20:36:19 +0700

    first basic setting

---
+++ simslider.js
@@ -0,0 +1,21 @@
+/*
+ * simSlider 
+ * jquery plugin for create slider / carousel (testing with jQuery 2.1.4)
+ * author: Willi-dev
+ * by willi-dev - willi.ilmukomputer@gmail.com
+ * Licensed under the MIT license
+ */
+
+(function ( $ ){
+
+	$.fn.simSlider = function( options ){
+
+		// set default options
+		var settings = $.extend({
+			width	: 
+
+		}, options);
+	};
+
+}(jQuery))
+ 
